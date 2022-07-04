78e44e28ac5ba4293fa7a92ceb1b5bb02d96904f
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-07-04 17:51:00 +0200

    connect wallet

---
+++ components/ConnectButton.tsx
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+const ConnectButton = () => {
+    return (
+        <Button colorScheme='green'>
+            Connect Wallet
+        </Button>
+    )
+}
+
+export default ConnectButton;
