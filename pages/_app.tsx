59bd2827fe0e32895893032d84a4e9f7cc33c27c
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-07-06 11:30:21 +0200

    adds favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react"
 import { Web3ReactProvider } from "@web3-react/core"
 import { Web3Provider } from "@ethersproject/providers"
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ethers } from "ethers";
 
 function getLibrary(provider: any, connector: any) {
@@ -11,6 +12,9 @@ function getLibrary(provider: any, connector: any) {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <link rel="shortcut icon" href="/labdao_favicon.ico"/>
+      </Head>
       <Web3ReactProvider getLibrary={getLibrary}>
         <Component {...pageProps} />
       </Web3ReactProvider>
